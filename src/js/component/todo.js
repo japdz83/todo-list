c1f850867afb2aa7e6f0bffd9a910acc508e795f
japdz83 <japdz83@gmail.com>
2020-08-05 21:25:41 +0000

    first commit

---
+++ src/js/component/todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Todo() {
+    return(
+        <div className="container">
+            <div className="row">
+                
+            </div>
+        </div>
+    );
+}
